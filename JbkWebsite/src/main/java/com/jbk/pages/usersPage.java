ce4bd78697ba457624fcfed0edb7e7bdf364aae1
poonam <poonamkakad49@gmail.com>
2020-02-26 23:03:46 +0530

    added userspage

--- JbkWebsite/src/main/java/com/jbk/pages/usersPage.java
+++ JbkWebsite/src/main/java/com/jbk/pages/usersPage.java
@@ -1,7 +1,10 @@
 package com.jbk.pages;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class usersPage {
-//@FindBy()
+@FindBy(xpath = "/html/body/div[1]/div[1]/section[2]/div/div/div/div[1]/a/button")
+WebElement adduser ;
+
 }
